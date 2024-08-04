d99444a63c7a8196cc606ddc3b84a8b987031ed7
Jacob Roeters van Lennep <jacob@roetersvanlennep.com>
2024-08-04 10:44:08 +0200

    Add samsung logo to header in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import samsungLogo from "./assets/samsung_white.png";
+import styled from "@emotion/styled";
+import {Grid} from "@mui/material";
 
+const StyledImage = styled.img`
+    height: 100px;
+    position: absolute;
+    top: 5px;
+    transform: translateX(-50%);
+`;
+
+
+const StyledLogoGrid = styled(Grid)`
+    height: 110px;
+`;
 
 function App() {
 
-    return <h1>Start of project</h1>
+
+    return (
+        <Grid container spacing={4} direction={"column"} alignItems="center">
+            <StyledLogoGrid item>
+                <StyledImage src={samsungLogo} alt={"samsung logo"}/>
+            </StyledLogoGrid>
+        </Grid>
+    )
 }
 
 export default App
